1a49b9ab7acaab27b48736b964db2861b1888890
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-18 16:56:17 -0400

    ui: launch combined macro and STEP core screen

--- app/src/main/java/com/medinaparra/freecadandroid/MainActivity.kt
+++ app/src/main/java/com/medinaparra/freecadandroid/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.medinaparra.freecadandroid.ui.MainScreenNative
+import com.medinaparra.freecadandroid.ui.MainScreenCore
 import com.medinaparra.freecadandroid.ui.theme.FreeCadAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FreeCadAndroidTheme {
-                MainScreenNative()
+                MainScreenCore()
             }
         }
     }
