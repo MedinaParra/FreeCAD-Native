b00423c834abb21f5bec9f823b9234a91f552333
MedinaParra <33606993+MedinaParra@users.noreply.github.com>
2026-07-18 10:58:15 -0400

    native: launch C++ device validation screen

--- app/src/main/java/com/medinaparra/freecadandroid/MainActivity.kt
+++ app/src/main/java/com/medinaparra/freecadandroid/MainActivity.kt
@@ -4,19 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.medinaparra.freecadandroid.ui.MainScreen
+import com.medinaparra.freecadandroid.ui.MainScreenNative
 import com.medinaparra.freecadandroid.ui.theme.FreeCadAndroidTheme
 
-/**
- * MainActivity is the primary entry point of the FreeCADAndroidNative application.
- */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FreeCadAndroidTheme {
-                MainScreen()
+                MainScreenNative()
             }
         }
     }
